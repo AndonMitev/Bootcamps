79edba0593486ef5179b89df47b2ef1eaab025d1
Andon Mitev <mitevandon94@gmail.com>
2020-02-03 22:52:15 +0200

    Add async middleware which resolve result or throw error to errorHandler middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,31 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const errorHandler = (err, req, res, next) => {
-  res.status(err.statusCode || 500).json({
+  let error = { ...err };
+
+  error.message = err.message;
+
+  // Mongoose bad object id
+  if (err.name === 'CastError') {
+    const message = `Bootcamp with id: ${err.value} not found`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Validation field failed
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || 'Server error'
+    error: error.message || 'Server error'
   });
 };
 
