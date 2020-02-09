5f7e68202bb22e6ad660cecdc1949c69ca56ef0d
Andon Mitev <mitevandon94@gmail.com>
2020-02-09 21:56:41 +0200

    Add find by fields and sort by fields

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,12 +1,39 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
+
+const EARTH_RADIUS_IN_MILES = 3963;
+const REMOVE_SELECT_FIELDS = ['select'];
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  const reqQuery = { ...req.query };
+  const excludeFields = ['select', 'sort'];
+
+  excludeFields.forEach(field => delete reqQuery[field]);
+
+  let queryStr = JSON.stringify(reqQuery);
+
+  queryStr = queryStr.replace(/\b(lt|lte|gt|gte|in)\b/, match => `$${match}`);
+
+  let query = Bootcamp.find(JSON.parse(queryStr));
+
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  const bootcamps = await query;
 
   res
     .status(200)
@@ -71,4 +98,26 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true });
+});
+
+// @desc      Get bootcamps in Radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Public
+exports.getBootcampsInRadius = asyncHandler(async (req, res) => {
+  const { zipcode, distance } = req.params;
+
+  const loc = await geocoder.geocode(zipcode);
+  const { latitude, longitude } = loc[0];
+
+  const radius = distance / EARTH_RADIUS_IN_MILES;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[longitude, latitude], radius] } }
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps
+  });
 });
